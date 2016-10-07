c851eaaf94bfbc1f6594c8e857bf464ff951293a
Elena Guandalini <elena.guandalini@tol.bz.it>
2016-10-07 17:14:57 +0200

    Add bundle for jsts

---
+++ src/main/java/jsts/JSTSBundle.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2001-2016 Territorium Online Srl. All Rights Reserved.
+ *
+ * This file contains Original Code and/or Modifications of Original Code as
+ * defined in and that are subject to the Territorium Online License Version
+ * 1.0. You may not use this file except in compliance with the License. Please
+ * obtain a copy of the License at http://www.tol.info/license/ and read it
+ * before using this file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS
+ * OR IMPLIED, AND TERRITORIUM ONLINE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT. Please see the
+ * License for the specific language governing rights and limitations under the
+ * License.
+ */
+package jsts;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * The {@link JSTSClientBundle} class
+ */
+public interface JSTSBundle extends ClientBundle {
+
+	static final JSTSBundle INSTANCE = GWT.create(JSTSBundle.class);
+
+	@Source("jsts.min.js")
+	TextResource jsts();
+}
