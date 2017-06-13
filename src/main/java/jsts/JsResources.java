137282c58fdf3271fe85da734b0aae0388bec1f4
Peter Zanetti <peter.zanetti@tol.bz.it>
2017-06-13 09:07:06 +0200

    Switch from OL v4.0.0 to OL v4.2.0

--- src/main/java/jsts/JsResources.java
+++ src/main/java/jsts/JsResources.java
@@ -1,18 +1,14 @@
 /*
  * Copyright (c) 2001-2016 Territorium Online Srl. All Rights Reserved.
  *
- * This file contains Original Code and/or Modifications of Original Code as
- * defined in and that are subject to the Territorium Online License Version
- * 1.0. You may not use this file except in compliance with the License. Please
- * obtain a copy of the License at http://www.tol.info/license/ and read it
- * before using this file.
+ * This file contains Original Code and/or Modifications of Original Code as defined in and that are subject to the
+ * Territorium Online License Version 1.0. You may not use this file except in compliance with the License. Please
+ * obtain a copy of the License at http://www.tol.info/license/ and read it before using this file.
  *
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS
- * OR IMPLIED, AND TERRITORIUM ONLINE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT. Please see the
- * License for the specific language governing rights and limitations under the
+ * The Original Code and all software distributed under the License are distributed on an 'AS IS' basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND TERRITORIUM ONLINE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+ * NON-INFRINGEMENT. Please see the License for the specific language governing rights and limitations under the
  * License.
  */
 package jsts;
@@ -23,12 +19,10 @@ import com.google.gwt.resources.client.TextResource;
 /**
  *
  * <p>
- * The <code>JsResources</code> interface is used to define the JavaScript files
- * as {@link TextResource}'s
+ * The <code>JsResources</code> interface is used to define the JavaScript files as {@link TextResource}'s
  * </p>
  * <p>
- * Copyright: 2003 - 2016 <a href="http://www.teritoriumonline.com">Territorium
- * Online Srl.</a>
+ * Copyright: 2003 - 2016 <a href="http://www.teritoriumonline.com">Territorium Online Srl.</a>
  * </p>
  * <p>
  * Via Buozzi 12, 39100 Bolzano, Italy.
@@ -47,6 +41,6 @@ public interface JsResources extends ClientBundle {
 	@Source("jsts.min.js")
 	TextResource jstsMin();
 
-	@Source("ol-4.0.0.js")
+	@Source("ol-4.2.0.js")
 	TextResource ol3();
 }
